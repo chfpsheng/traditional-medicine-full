cd9605f125102f5525a4586ae534d50e2a119e28
chfpsheng <13343617+chfpsheng11@user.noreply.gitee.com>
2025-12-08 06:49:13 +0800

    修改依赖

--- electron/vite.config.js
+++ electron/vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import electron from 'vite-plugin-electron'
-import electronRenderer from 'vite-plugin-electron-renderer'
 
 export default defineConfig({
   plugins: [
-    vue(),
-    electron({
-      main: {
-        entry: 'main.js',
-      },
-    }),
-    electronRenderer(),
+    vue()
   ],
   server: {
     port: 3000,
